a2c398737090da2ff5ee96bfd8d07b2a1ef2163b
arifhnd <forevertkj4@gmail.com>
2021-04-06 14:33:09 +0700

    change description

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn is funnnnn 😭😭
         </a>
       </header>
     </div>
